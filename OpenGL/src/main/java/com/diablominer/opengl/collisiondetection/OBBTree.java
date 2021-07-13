f6f16ce8de317063d7b7bc4c23446a1505ee6a6b
DialboMiner <martinrussler3@gmail.com>
2021-07-13 17:02:04 +0200

    Implemented a more modular version of the OBBTree constructor

--- OpenGL/src/main/java/com/diablominer/opengl/collisiondetection/OBBTree.java
+++ OpenGL/src/main/java/com/diablominer/opengl/collisiondetection/OBBTree.java
@@ -7,22 +7,31 @@ import java.util.List;
 
 public class OBBTree {
 
-    private OBBTreeNode[] nodes;
+    private final OBBTreeNode[] nodes;
 
     public OBBTree(List<Vector3f> points, int levels) {
-        int size = 0x2 << levels;
+        int size = (1 << levels) - 1;
         if (size < 0) {
             throw new IllegalArgumentException("The argument chosen for levels caused the size of the array to overflow. Please choose a smaller argument.");
         }
         nodes = new OBBTreeNode[size];
 
-        nodes[0] = new OBBTreeNode(points);
-        ArrayList<ArrayList<Vector3f>> twoPointCollections = splitPointsInHalf(nodes[0], nodes[0].getPoints());
-        nodes[1] = new OBBTreeNode(twoPointCollections.get(0));
-        nodes[2] = new OBBTreeNode(twoPointCollections.get(1));
+        for (int i = 0; i < ((size - 2) / 2); i++) {
+            if (i == 0) {
+                nodes[0] = new OBBTreeNode(points);
+                ArrayList<ArrayList<Vector3f>> twoPointSets = splitPointsInHalf(nodes[0]);
+                nodes[1] = new OBBTreeNode(twoPointSets.get(0));
+                nodes[2] = new OBBTreeNode(twoPointSets.get(1));
+            } else {
+                ArrayList<ArrayList<Vector3f>> twoPointSets = splitPointsInHalf(nodes[(int) Math.floor((i - 1) / 2.0)]);
+                nodes[2 * i + 1] = new OBBTreeNode(twoPointSets.get(0));
+                nodes[2 * i + 2] = new OBBTreeNode(twoPointSets.get(1));
+            }
+
+        }
     }
 
-    private ArrayList<ArrayList<Vector3f>> splitPointsInHalf(OBBTreeNode obbTreeNode, List<Vector3f> points) {
+    private ArrayList<ArrayList<Vector3f>> splitPointsInHalf(OBBTreeNode obbTreeNode) {
         Vector3f centerPoint = obbTreeNode.getCenterPoint();
         Vector3f longestAxis = obbTreeNode.getLongestAxis();
         Vector3f middleAxis = obbTreeNode.getMiddleAxis();
@@ -38,7 +47,7 @@ public class OBBTree {
 
         ArrayList<Vector3f> positiveSide = new ArrayList<>();
         ArrayList<Vector3f> negativeSide = new ArrayList<>();
-        for (Vector3f point : points) {
+        for (Vector3f point : obbTreeNode.getPoints()) {
             if ((point.dot(normalVector) - distance) >= 0.0f) {
                 positiveSide.add(point);
             } else {
